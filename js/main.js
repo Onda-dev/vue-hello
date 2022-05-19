f626b63c4dfaea8461925a3a745692660803f1df
Onda-dev <manuelaltieri3@gmail.com>
2022-05-19 14:42:52 +0200

    Punto 1 ok

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        message: "Hello"
+    }
+})
